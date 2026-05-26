359ca769a0f81d93b22608844a45cdac3ba96167
rehanjatu <rehanjatu505@gmail.com>
2026-05-26 12:22:10 +0530

    add initial file

---
+++ tom.php
@@ -0,0 +1,12 @@
+<?php
+// kid.php
+
+$name = "Tommy";
+$age = 10;
+$favoriteColor = "Blue";
+
+echo "<h1>Kid Profile</h1>";
+echo "<p><strong>Name:</strong> $name</p>";
+echo "<p><strong>Age:</strong> $age</p>";
+echo "<p><strong>Favorite Color:</strong> $favoriteColor</p>";
+?>
