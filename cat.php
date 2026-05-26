6f27a0c8dd554a7d297fdf80feabe238059b8fa0
rehanjatu <rehanjatu505@gmail.com>
2026-05-26 12:31:46 +0530

    add new file

---
+++ cat.php
@@ -0,0 +1,12 @@
+<?php
+// cat.php
+
+$name = "Whiskers";
+$age = 3;
+$color = "Orange";
+
+echo "<h1>Cat Profile</h1>";
+echo "<p><strong>Name:</strong> $name</p>";
+echo "<p><strong>Age:</strong> $age . " years old</p>";
+echo "<p><strong>Color:</strong> $color</p>";
+?>
